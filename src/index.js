c030bcf65d8c52ce11eb3aa572e85950a645137a
photophactory <ahemenkelvin2@gmail.com>
2021-10-22 14:39:45 +0100

    update commit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import {robots} from './robots';
 
 ReactDOM.render(<App />,  document.getElementById('root'));
 
