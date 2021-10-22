f5f9bae23ffd09a46cbfad14f58d68aebc15df7f
photophactory <ahemenkelvin2@gmail.com>
2021-10-22 13:38:35 +0100

    initial commit

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Stuffy Essaar',
+        username: 'Stuffy',
+        email: 'essaar@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'David Teddy',
+        username: 'Teddy',
+        email: 'teddy@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Kevin Rhodes',
+        username: 'Rhodes',
+        email: 'rhodes@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Seun Container',
+        username: 'Contdotjs',
+        email: 'halfstack@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Francis Oheji',
+        username: 'Abiphix',
+        email: 'thetwoOjays@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Kevin Israel',
+        username: 'Icon',
+        email: 'confusedkevin@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'Kevin Ansi',
+        username: 'Kevans',
+        email: 'thekevanstv@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'Benita ionno',
+        username: 'interngen',
+        email: 'interngen@gmail.com'
+    },
+    {
+        id: 9,
+        name: 'Victor Weka',
+        username: 'Atall',
+        email: 'atall@gmail.com'
+    },
+    {
+        id: 10,
+        name: 'Patrick Oche',
+        username: 'Fullstack',
+        email: 'mrbsu2021@gmail.com'
+    },
+]
